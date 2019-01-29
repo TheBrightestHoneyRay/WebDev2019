12d8f1c11ce577594233233ae22d47d17877d3f3
TheBrightestHoneyRay <31367887+TheBrightestHoneyRay@users.noreply.github.com>
2019-01-29 15:13:21 +0600

    JavaScript

---
+++ Week1/CodeCademy/JavaScript/log.js
@@ -0,0 +1,29 @@
+//stage 4
+console.log('JavaScript');
+console.log(2011);
+console.log('Woohoo! I love to code! #codecademy');
+console.log(20.49)
+
+//stage 5
+console.log(18 + 3.5)
+console.log(2019-1969)
+console.log(65/240)
+console.log(0.2708*100)
+
+//stage 6
+console.log('Hello'+' '+'World')
+
+//stage 7
+console.log('Teaching the world how to code'.length)
+
+//stage 8
+// Use .toUpperCase() to log 'Codecademy' in all uppercase letters
+console.log('Codecademy'.toUpperCase());
+
+// Use a string method to log the following string without whitespace at the beginning and end of it.
+console.log('    Remove whitespace   '.trim());
+
+//stage 9
+console.log(Math.floor(Math.random()*100));
+console.log(Math.ceil(43.8))
+console.log(Number.isInteger(2017))
