fab69d3142183b59e4c21f75d9285cdeeb09ff0c
TheBrightestHoneyRay <31367887+TheBrightestHoneyRay@users.noreply.github.com>
2019-02-10 17:49:11 +0600

    Update variables.js

--- Week1/CodeCademy/JavaScript/variables.js
+++ Week1/CodeCademy/JavaScript/variables.js
@@ -2,3 +2,56 @@ var favoriteFood = 'pizza';
 var numOfSlices = 8;
 console.log(favoriteFood);
 console.log(numOfSlices);
+
+// stage 3
+let changeMe = true
+changeMe = false
+console.log(changeMe)
+
+//stage 4
+const entree = 'Enchiladas'
+console.log(entree)
+
+//stage 5
+let levelUp = 10;
+let powerLevel = 9001;
+let multiplyMe = 32;
+let quarterMe = 1152;
+
+// Use the mathematical assignments in the space below:
+levelUp+=5
+powerLevel-=100
+multiplyMe*=11
+quarterMe/=4
+
+// These console.log() statements below will help you check the values of the variables.
+// You do not need to edit these statements. 
+console.log('The value of levelUp:', levelUp); 
+console.log('The value of powerLevel:', powerLevel); 
+console.log('The value of multiplyMe:', multiplyMe); 
+console.log('The value of quarterMe:', quarterMe);
+
+//stage 6
+let gainedDollar = 3;
+let lostDollar = 50;
+
+gainedDollar++
+lostDollar--
+
+//stage 7
+var favoriteAnimal = "Lion"
+console.log("My favorite animal: " + favoriteAnimal)
+
+//stage 8
+let myName = 'Natalia';
+let myCity = 'Mexico City';
+
+console.log(`My name is ${myName}. My favorite city is ${myCity}.`)
+
+//stage 9
+let newVariable = 'Playing around with typeof.';
+
+console.log(typeof(newVariable))
+newVariable = 1
+
+console.log(typeof(newVariable))
